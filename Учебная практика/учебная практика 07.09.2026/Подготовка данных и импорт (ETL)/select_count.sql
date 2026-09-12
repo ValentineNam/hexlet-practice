335e133d1cdf4cec8658d891c1c74eed51b3e5a0
v.nam <v4lentine.nam@gmail.com>
2026-09-12 17:40:02 +0300

    fix queries select count

--- Учебная практика/учебная практика 07.09.2026/Подготовка данных и импорт (ETL)/select_count.sql
+++ Учебная практика/учебная практика 07.09.2026/Подготовка данных и импорт (ETL)/select_count.sql
@@ -1,15 +1,16 @@
 -- Запросы для подсчета количества записей в таблицах
 SELECT COUNT(*) AS partners_count FROM partners;
 SELECT COUNT(*) AS products_count FROM products;
-SELECT COUNT(*) AS sales_count FROM sales_history;
+SELECT COUNT(*) AS shipments_count FROM shipments;
+SELECT COUNT(*) AS shipment_items_count FROM shipment_items;
 
 -- Запросы для проверки ссылочной целостности
-SELECT s.sale_id, s.partner_id
-FROM sales_history s
+SELECT s.shipment_id, s.partner_id
+FROM shipments s
 LEFT JOIN partners p ON p.partner_id = s.partner_id
 WHERE p.partner_id IS NULL;
 
-SELECT s.sale_id, s.product_id
-FROM sales_history s
-LEFT JOIN products p ON p.product_id = s.product_id
+SELECT si.shipment_item_id, si.product_id
+FROM shipment_items si
+LEFT JOIN products p ON p.product_id = si.product_id
 WHERE p.product_id IS NULL;
